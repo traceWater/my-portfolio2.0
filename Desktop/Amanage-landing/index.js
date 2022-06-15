e3948966ad70b7804ba809d2c6878e137b56c549
traceWater <dalecottrell62@gmail.com>
2022-06-15 15:21:36 -0700

    initial commi

---
+++ Desktop/Amanage-landing/index.js
@@ -0,0 +1,89 @@
+const hamburger = document.querySelector("#hamburger")
+const mobileMenu = document.querySelector(".mobilemenu")
+const slideCards = document.querySelectorAll(".card")
+
+ const sliderIndicatorParent = document.querySelector("#slideIndicator"),
+    sliderIndicator = sliderIndicatorParent.querySelectorAll("span")
+const form = document.querySelector("form"),
+    email = form.querySelector("#email"),
+    errorTxt = form.querySelector("#errortxt")
+
+
+    //mobile menu
+hamburger.addEventListener("click", ()=>{
+    mobileMenu.classList.toggle("show")
+    mobileMenu.classList.add("no-scroll")
+
+    if (mobileMenu.classList.contains("show")) {
+        hamburger.src = "images/icon-close.svg"
+    }else{
+        hamburger.src = "images/icon-hamburger.svg"
+        mobileMenu.classList.remove("no-scroll")
+    }
+})
+
+//slider
+
+let slideIndex = 0
+
+const slideTestimonial = () => {
+    
+    if(window.screen.width > 768){
+        return
+    }
+    slideIndex++
+
+    if(slideIndex > slideCards.length - 1){
+        slideIndex = 0
+    }
+    slideCards.forEach((slide) => {
+        slide.classList.add("hidden")
+    })
+    slideCards[slideIndex].classList.remove("hidden")
+
+    sliderIndicator.forEach(slider => {
+        slider.style.backgroundColor = "white"
+    })
+    if (slideIndex === 0) {
+        one.style.backgroundColor = "hsl(12, 88%, 59%)"
+    }
+    if (slideIndex === 1) {
+        two.style.backgroundColor = "hsl(12, 88%, 59%)"
+    }
+    if (slideIndex === 2) {
+        three.style.backgroundColor = "hsl(12, 88%, 59%)"
+    }
+    if (slideIndex === 3) {
+        four.style.backgroundColor = "hsl(12, 88%, 59%)"
+    }
+
+}
+setInterval(() => slideTestimonial(), 3000);
+
+//form validation
+
+form.addEventListener("submit", e => {
+    e.preventDefault()
+
+    checkEmail()
+
+    email.onkeyup = () => checkEmail()
+
+    function checkEmail() {
+        let eValue = email.value
+        let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/; //pattern for validate email
+
+        if (eValue === "") {
+            email.classList.add("error")
+            errorTxt.textContent = "Please enter your email"
+        }else if(!eValue.match(pattern)){
+            email.classList.add("error")
+            errorTxt.textContent = "Please insert a valid email"
+        }else {
+            email.classList.remove("error")
+            errorTxt.textContent = ""
+        }
+    }
+})
+
+slideTestimonial()
---
+++ Desktop/Amanage-landing/tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+    },
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+    },
+    colors: {
+      brightRed: "hsl(12, 88%, 59%)",
+      brightRedLight: "hsla(12, 88%, 59%, 0.7)",
+      shadowRed: "hsla(12, 88%, 59%, 0.4)",
+      lightRed: "hsl(12, 77%, 76%)",
+      darkBlue: "hsl(228, 39%, 23%)",
+      darkGrayishBlue: "hsl(227, 12%, 61%)",
+      veryDarkBlue: "hsl(233, 12%, 13%)",
+      veryPaleRed: "hsl(13, 100%, 96%)",
+      veryLightGray: "hsl(0, 0%, 98%)",
+    },
+    fontFamily: {
+      vietnam: ["'Be Vietnam Pro', 'sans-serif'"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};

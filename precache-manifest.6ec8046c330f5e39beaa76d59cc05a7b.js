c27ce2a43f5b1837af881e8c7524a8ed7d79720d
traceWater <dalecottrell62@gmail.com>
2022-08-11 12:22:26 -0700

    Updates

---
+++ precache-manifest.6ec8046c330f5e39beaa76d59cc05a7b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "af44b7cb5dfafbc98ad2",
+    "url": "/my-portfolio2.0/static/css/main.af138d9e.chunk.css"
+  },
+  {
+    "revision": "af44b7cb5dfafbc98ad2",
+    "url": "/my-portfolio2.0/static/js/main.af44b7cb.chunk.js"
+  },
+  {
+    "revision": "b0729a2c2a6da268381b",
+    "url": "/my-portfolio2.0/static/js/1.b0729a2c.chunk.js"
+  },
+  {
+    "revision": "1aa2f3d666430624cf7e",
+    "url": "/my-portfolio2.0/static/js/runtime~main.1aa2f3d6.js"
+  },
+  {
+    "revision": "d76f2dfb4ee3f4a545e6fe6054cca994",
+    "url": "/my-portfolio2.0/static/media/dale.d76f2dfb.png"
+  },
+  {
+    "revision": "9130429c830fa0818d651395c45ee03f",
+    "url": "/my-portfolio2.0/static/media/alphaworks1.9130429c.png"
+  },
+  {
+    "revision": "1c59900b217964d6783298600c80e88d",
+    "url": "/my-portfolio2.0/static/media/cabbagetreelandscape.1c59900b.png"
+  },
+  {
+    "revision": "654c8a2e2dacabbac4f4297a8655cfb6",
+    "url": "/my-portfolio2.0/static/media/starman.654c8a2e.png"
+  },
+  {
+    "revision": "40beaa917f634d91aca28591ce6d75c9",
+    "url": "/my-portfolio2.0/index.html"
+  }
+];
